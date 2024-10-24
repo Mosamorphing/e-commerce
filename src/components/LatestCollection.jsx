5a06447c6360092ab0c085b6f0dc31d7d93c1ac6
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2024-10-24 13:55:03 +0100

    best seller section is in order

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -19,7 +19,7 @@ const LatestCollection = () => {
         <div className='text-center py-8 text-3x1'>
             <Title text1={'LATEST'} text2={'COLLECTIONS'} />
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-            Fashion started when humans began wearing clothes. These clothes were typically made from plants, animal skins and bone. Before the mid-19th century the division between haute couture and ready-to-wear did not really exist. But the most basic pieces of female clothing were made-to-measure by dressmakers and seamstresses dealing directly with the client. Most often, clothing was patterned, sewn and tailored in the household. When storefronts appeared selling ready-to-wear clothing, this need was removed from the domestic workload.
+            Fashion started when humans began wearing clothes. These clothes were typically made from plants, animal skins and bone. Before the mid-19th century the division between haute couture and ready-to-wear did not really exist. 
             </p>
         </div>
 
