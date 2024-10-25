15b3b46b0051250aec32f9f146f40b2e97d07c90
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2024-10-25 11:44:48 +0100

    product sorting is in order; users can now sort products from the lowest price to highest price

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -11,6 +11,7 @@ const Collection = () => {
     const [filterProducts,setFilterProducts] = useState([]);
     const [category,setCategory] = useState([]);
     const [subCategory,setSubCategory] = useState([]);
+    const [sortType,setSortType] = useState('relevant');
 
     const toggleCategory = (e) => {
 
@@ -45,9 +46,32 @@ const Collection = () => {
         setFilterProducts(productsCopy)
     }
 
+    const sortProduct = () => {
+        let fpCopy = filterProducts.slice();
+
+        switch (sortType) {
+            case 'low-high':
+            setFilterProducts(fpCopy.sort((a,b)=>(a.price - b.price)));
+            break;
+
+            case 'high-low':
+            setFilterProducts(fpCopy.sort((a,b)=>(b.price - a.price)));
+            break;
+
+            default:
+                applyFilter();
+                break;
+        }
+    }
+
+
     useEffect (()=> {
             applyFilter();
     },[category,subCategory])
+
+    useEffect (() => {
+        sortProduct();
+    },[sortType])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -97,7 +121,7 @@ const Collection = () => {
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
             <Title text1={'ALL'} text2={'COLLECTIONS'} />
             {/* Product Sorting */}
-            <select className='border-2 border-gray-300 text-sm px-2'>
+            <select onChange={(e)=>setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
                 <option value='relevant'>Sort by: Relevant</option>
                 <option value='low-high'>Sort by: Low-high</option>
                 <option value='high-low'>Sort by: High-low</option>
